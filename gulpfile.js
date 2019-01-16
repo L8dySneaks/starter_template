0dc1e5b71afa0177c3972569db73f5b2c089ceb6
Symosko <dks0515936@depc02wq0lrhtdd.dcsg.com>
2019-01-16 11:43:31 -0500

    switching to new gulp 4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+const { series, parallel } = require('gulp');
 
-//  With this task you would need to type gulp 'task-name' everytime you want to compile CSS
+function clean(cb) {
+    //body omitted
+    cb();
+}
+function css(cb) {
+    //body omitted
+    cb();
+}
 
-gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss')  // Gets all files ending with .scss in app/scss and children directories
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))  //outputs compiled CSS to the css directory
-});
+function javascript(cb) {
+    //body omitted
+    cb();
+}
 
-// instead you can create a watch task that will run every time the file changes
-
-gulp.task('watch', function(){
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-});
+exports.build = series(clean, parallel(css, javascript));
