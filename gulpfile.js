8d83e934f4139a92788c9d8a9c3bdb449a6e9856
Symosko <dks0515936@depc02wq0lrhtdd.dcsg.com>
2019-01-29 10:48:49 -0500

    last update, moving on from gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,70 @@
-var gulp = require('gulp');
-const { series, parallel } = require('gulp');
+//Load GULP
+const { gulp, src, task, dest, series, parallel } = require('gulp');
+var watch = require('gulp-watch');
 
-function clean(cb) {
-    //body omitted
-    cb();
-}
-function css(cb) {
-    //body omitted
-    cb();
+// CSS Plugins
+var sass = require('gulp-sass');
+
+//Browser Plugins
+var browserSync = require('browser-sync').create();
+
+// Project Variables
+var styleWatch = './app/scss/**/*.scss';
+var htmlWatch = './app/*.html';
+
+//Tasks
+
+gulp.task('default', function() {
+    return gulp.src('sass/*.scss')
+      .pipe(watch('sass/*.scss'))
+      .pipe(sass())
+      .pipe(gulp.dest('dist'));
+  });
+  
+// Task 1 : Have browser load
+function bs(done) {
+    browserSync.init({
+        server: {
+            baseDir: 'app/'
+        }
+    });
+    done();
 }
 
-function javascript(cb) {
-    //body omitted
-    cb();
+// Task 2 : Have browser reload on change - watch HTML and SCSS files - for dev purposes
+function reload(done) {
+    browserSync.reload();
+    done();
 }
 
-exports.build = series(clean, parallel(css, javascript));
+// Task  : Files to watch are HTML and CSS   *** Need to convert SCSS to CSS
+function watchFiles(done){
+    watch(styleWatch, reload);
+}
+
+// Task 3 : Minify css and js files
+
+// Task 4 : spit min files to dist folder
+
+
+
+
+// function buildCss(callback) {
+//     //execute scripts
+//     return src(styleWatch)
+//         .pipe(sass())
+//         .pipe(dest('app/css'))
+
+//     callback();
+// }
+
+// function watchFiles() {
+//     watch(styleWatch, series(css, reload));
+// }
+
+
+// Define TASKS
+// task("default", series(watchFiles, buildCss)); 
+// exports.dev = parallel(bs);
+exports.watch = parallel(bs, watchFiles);
+
